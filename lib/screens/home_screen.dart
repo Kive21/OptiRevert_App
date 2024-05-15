488939d0fe7df2da9e8e3ba310329a42f941460f
Eric Kiverenge <avwengae@gmail.com>
2024-05-15 14:34:19 +0300

    added const where applicable

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -10,7 +10,7 @@ class HomeScreen extends StatelessWidget {
         title: Text('OptiRevert'),
       ),
       body: DashboardScreen(),
-      drawer: CustomDrawer(),
+      drawer: const CustomDrawer(),
     );
   }
 }
